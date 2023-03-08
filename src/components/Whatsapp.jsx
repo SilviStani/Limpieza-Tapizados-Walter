9f4e934176556c8fed1079db4eb5f871fcceb03a
Silvina Staniszewski <silvina.stani@gmail.com>
2023-03-08 09:53:18 -0300

    wme

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -17,7 +17,7 @@ filter: drop-shadow(3px 4px 3px gray);
 
 const Whatsapp = () => {
   return (
-    <WhatsApp><a href="https://wa.me/+5491163632288/?text=I'm%20interested%20in%20your%20car%20for%20sale" target="_blank" rel="noopener noreferrer"><Image src="/assets/whatsapp.png"/></a></WhatsApp>
+    <WhatsApp><a href="https://wa.me/5491163632288/?text=I'm%20interested%20in%20your%20car%20for%20sale" target="_blank" rel="noopener noreferrer"><Image src="/assets/whatsapp.png"/></a></WhatsApp>
   )
 }
 
