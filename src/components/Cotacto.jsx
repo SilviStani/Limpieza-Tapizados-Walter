71de0c0edf731c3888452b57d397abfd31180c48
Silvina Staniszewski <silvina.stani@gmail.com>
2023-03-08 09:41:28 -0300

    wme

--- src/components/Cotacto.jsx
+++ src/components/Cotacto.jsx
@@ -1,12 +1,31 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function Contacto() {
+function FormDisabledExample() {
   return (
-    <div>
-        Formulario de Contacto
-    </div>
+    <Form>
+      <fieldset disabled>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
+          <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+          <Form.Select id="disabledSelect">
+            <option>Disabled select</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            type="checkbox"
+            id="disabledFieldsetCheck"
+            label="Can't check this"
+          />
+        </Form.Group>
+        <Button type="submit" href="https://wa.me/1163632288?text=I'm%20interested%20in%20your%20car%20for%20sale" target="_blank" rel="noopener noreferrer">Send</Button>
+      </fieldset>
+    </Form>
   );
 }
 
-export default Contacto;
+export default FormDisabledExample;
