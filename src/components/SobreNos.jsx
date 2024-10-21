bea74cd28891c440b4ab7e43707cea81d783db5b
Silvina Staniszewski <silvina.stani@gmail.com>
2024-10-21 08:04:08 -0300

    refining responsiveness

--- src/components/SobreNos.jsx
+++ src/components/SobreNos.jsx
@@ -21,14 +21,14 @@ const Trabajo = styled.div`
 
 const Titulo = styled.h2`
   height: 20px;
-  color: #65647C;
+  color: #65647c;
   font-size: 25px;
   text-align: center;
   @media screen and (max-width: 770px) {
     font-size: 20px;
   }
-  `;
-  const SubTitulo = styled.h3`
+`;
+const SubTitulo = styled.h3`
   height: 20px;
   font-size: 18px;
   @media screen and (max-width: 770px) {
@@ -42,9 +42,9 @@ const Steps = styled.div`
   align-items: center;
   width: 100%;
   height: 85%;
-  `;
-  
-  const StepiStep = styled.div`
+`;
+
+const StepiStep = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -54,12 +54,13 @@ const Steps = styled.div`
   border-radius: 100%;
   border: 2px dashed crimson;
   @media screen and (max-width: 770px) {
-    border: none;  
+    height: 100px;
+    border: 1px dashed crimson;
     justify-content: center;
   }
-  `;
-  
-  const Pago = styled.div`
+`;
+
+const Pago = styled.div`
   width: 100%;
   height: 30%;
   display: flex;
@@ -68,21 +69,21 @@ const Steps = styled.div`
   @media screen and (max-width: 770px) {
     width: 100%;
   }
-  `;
-  
-  const Preguntas = styled.div`
+`;
+
+const Preguntas = styled.div`
   width: 100%;
   height: 30%;
   display: flex;
   align-items: center;
   justify-content: space-around;
-  `;
-  
-  const Image = styled.img`
+`;
+
+const Image = styled.img`
   height: 30px;
   transform: rotate(25deg);
   @media screen and (max-width: 770px) {
-    height: 20px;
+    height: 15px;
   }
 `;
 
@@ -91,41 +92,40 @@ const Desc = styled.p`
   font-family: "monospace";
   font-weight: bold;
   @media screen and (max-width: 770px) {
-    fonst-size: 15px;
+    font-size: 13px;
   }
-  `;
+`;
 
 const Contain = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-border-top: 3px dashed black; 
-border-bottom: 3px dashed black;
-@media screen and (max-width: 770px) {
-  border-top: none; 
-}
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-top: 3px dashed black;
+  border-bottom: 3px dashed black;
+  @media screen and (max-width: 770px) {
+    border-top: none;
+  }
 `;
 
 const Video = styled.video`
-height: 220px;
-@media screen and (max-width: 770px) {
-  height: 100px; 
-}
-@media screen and (max-width: 1445px) {
-  height: 130px; 
-}
+  height: 220px;
+  @media screen and (max-width: 770px) {
+    height: 100px;
+  }
+  @media screen and (max-width: 1445px) {
+    height: 130px;
+  }
 `;
 
-const Ul = styled.ul` 
-`;
+const Ul = styled.ul``;
 
 const Li = styled.li`
-color: #65647C;
-@media screen and (max-width: 770px) {
- font-size: 13px;   
-}
+  color: #65647c;
+  @media screen and (max-width: 770px) {
+    font-size: 13px;
+  }
 `;
 
 const SobreNos = () => {
@@ -164,30 +164,26 @@ const SobreNos = () => {
         </Steps>
       </Trabajo>
       <Pago>
-        <SubTitulo>Formas de <br /> Pago</SubTitulo>
-        <Video 
-            src = "/assets/pago.mp4"
-            autoPlay
-            loop
-            muted/>
-            <Ul>
-              <Li>Mercado Pago</Li>
-              <Li>Efectivo</Li>
-              <Li>Transferencia Bancaria</Li>
-            </Ul>
+        <SubTitulo>
+          Formas de <br /> Pago
+        </SubTitulo>
+        <Video src="/assets/pago.mp4" autoPlay loop muted />
+        <Ul>
+          <Li>Mercado Pago</Li>
+          <Li>Efectivo</Li>
+          <Li>Transferencia Bancaria</Li>
+        </Ul>
       </Pago>
       <Preguntas>
-      <Ul>
-              <Li>El servicio dura entre 60 y 90 minutos</Li>
-              <Li>Higieniza y Desinfecta</Li>
-              <Li>Los Productos no son Tóxicos</Li>
-              <Li>Las manchas salen? <span style={{color: "crimson"}}>SI!!</span></Li>
-            </Ul>
-      <Video 
-            src = "/assets/consulta.mp4"
-            autoPlay
-            loop
-            muted/>
+        <Ul>
+          <Li>El servicio dura entre 60 y 90 minutos</Li>
+          <Li>Higieniza y Desinfecta</Li>
+          <Li>Los Productos no son Tóxicos</Li>
+          <Li>
+            Las manchas salen? <span style={{ color: "crimson" }}>SI!!</span>
+          </Li>
+        </Ul>
+        <Video src="/assets/consulta.mp4" autoPlay loop muted />
       </Preguntas>
     </Container>
   );
